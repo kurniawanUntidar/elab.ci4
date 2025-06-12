4570c44ceba26a1bce5251c1d40e6f3f743397b3
kurniawanUntidar <dwi.kurniawan@untidar.ac.id>
2025-06-12 15:58:34 +0700

    change icon

--- app/Views/pages/Dashboard.php
+++ app/Views/pages/Dashboard.php
@@ -16,7 +16,7 @@
                     <!-- Content Row -->
                     <div class="row">
 
-                        <!-- Earnings (Monthly) Card Example -->
+                        <!-- Card Jumlah User -->
                         <div class="col-xl-3 col-md-6 mb-4">
                             <div class="card border-left-primary shadow h-100 py-2">
                                 <div class="card-body">
@@ -27,7 +27,7 @@
                                             <div class="h5 mb-0 font-weight-bold text-gray-800"><?= sizeof($users); ?></div>
                                         </div>
                                         <div class="col-auto">
-                                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                                            <i class="fas fa-users fa-2x text-gray-300"></i>
                                         </div>
                                     </div>
                                 </div>
